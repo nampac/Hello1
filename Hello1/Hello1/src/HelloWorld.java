65d8a5e01f75d922a1e5dce8324bba0ebcc19961
Alexander Mosena <alexmosena@yahoo.de>
2011-09-18 14:10:16 +0200

    Char f Ausgabe hinzugefügt

--- Hello1/Hello1/src/HelloWorld.java
+++ Hello1/Hello1/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import javax.sound.midi.SysexMessage;
+
 
 public class HelloWorld {
 
@@ -6,12 +8,18 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		StringBuilder builder = new StringBuilder("Hello World ");
+		StringBuffer b1 = new StringBuffer();
+		b1.append("f");
+		
 		if(args.length != 0){
 			for(String s:args){
 				builder.append(s);
 			}
 		}
 		System.out.println(builder.toString());
+		System.out.println(b1.toString());
+		System.out.println();
+		
 		
 		// TODO Auto-generated method stub
 
